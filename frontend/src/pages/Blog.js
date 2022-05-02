8fc86851211f61a4a10de2f8b82719cdcab7faff
Courtney <courtskit@gmail.com>
2022-05-02 13:17:33 -0700

    set up files

---
+++ frontend/src/pages/Blog.js
@@ -0,0 +1,9 @@
+export const Blog = () => {
+  return (
+    <div>
+      <h1> This is the Blog Page</h1>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ frontend/src/pages/Listing.js
@@ -0,0 +1,9 @@
+export const Listing = () => {
+  return (
+    <div>
+      <h1> This is the Listing</h1>
+    </div>
+  );
+};
+
+export default Listing;
