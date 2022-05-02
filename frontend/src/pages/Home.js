557bd46ff107b4a6fd6e59477848d98d45664eeb
Courtney <courtskit@gmail.com>
2022-05-02 14:09:14 -0700

    home file structure set up

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,38 @@
 export const HomePage = () => {
   return (
     <div>
-      <h1> This is the HomePage</h1>
+
+      <section>
+        <h1>Search for your next adventure.</h1>
+        <form>
+          <input type="search" name="locationSearch" placeholder="Search Locations"/>
+        </form>
+      </section>
+
+      <section>
+        <img src= "https://www.adventure-journal.com/wp-content/uploads/2016/06/canyonlands-national-park-map-Feat.jpg" />
+      </section>
+
+      <section>
+        <h2>Most Popular</h2>
+      </section>
+
+      <section>
+        <h2>Boondock Sites</h2>
+      </section>
+
+      <section>
+        <h2>Featured Parks</h2>
+      </section>
+
+      <section>
+        <img src="https://via.placeholder.com/150"></img>
+        <img src="https://via.placeholder.com/150"></img>
+        <img src="https://via.placeholder.com/150"></img>
+        <img src="https://via.placeholder.com/150"></img>
+        <img src="https://via.placeholder.com/150"></img>
+      </section>
+      
     </div>
   );
 };
