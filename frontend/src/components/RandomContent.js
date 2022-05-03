6b7eabacd5016affe249fdf8484b73f03e71d845
Marcin Swaltek <swaltek@tutanota.com>
2022-05-03 16:42:23 -0400

    basic carousel css

--- frontend/src/components/RandomContent.js
+++ frontend/src/components/RandomContent.js
@@ -1,13 +1,18 @@
+import Carousel from './Carousel';
 const RandomContent = (props) => {
     return(
         <div>
-            <section>
+            <Carousel>
             <img src="https://via.placeholder.com/150"></img>
             <img src="https://via.placeholder.com/150"></img>
             <img src="https://via.placeholder.com/150"></img>
             <img src="https://via.placeholder.com/150"></img>
             <img src="https://via.placeholder.com/150"></img>
-            </section>
+            <img src="https://via.placeholder.com/150"></img>
+            <img src="https://via.placeholder.com/150"></img>
+            <img src="https://via.placeholder.com/150"></img>
+            <img src="https://via.placeholder.com/150"></img>
+            </Carousel>
         </div>
     );
 }
