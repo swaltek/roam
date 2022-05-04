9a778df810b7fc584b7d4bd3808e70f9722da063
philliphall131 <phillip.hall131@gmail.com>
2022-05-04 15:06:54 -0500

    updated some api functions, fixed a few bugs

--- frontend/src/api/apiHelpers.js
+++ frontend/src/api/apiHelpers.js
@@ -4,7 +4,6 @@ const apiHelpers = { }
 
 apiHelpers.getCsrfConfig = () => {
   return {
-    // needs to exist with separate projects 
     withCredentials: true, 
     headers: {
       'X-CSRFToken': Cookie.get("csrftoken")
@@ -13,11 +12,8 @@ apiHelpers.getCsrfConfig = () => {
 }
 
 apiHelpers.tryCatchFetch = async (axiosCall) => {
-  // async always returns a promise
   try {
-    //await- wait for promise. similar to .then
     const response = await axiosCall() 
-    // this doesnt look like a promise but gets converted to a promise via async ... success avoids returning null
     return response.data ? response.data : { message:"success" } 
   }
   catch (e) {
