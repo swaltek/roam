e6e21775fd6c1bf186121420370e2292e57d5aea
Marcin Swaltek <swaltek@tutanota.com>
2022-05-04 07:27:21 -0400

    basic mapbox implementation

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,12 +1,51 @@
-const Map = (props) => {
+import { useEffect, useState, useRef } from 'react';
+import '../styles/map.css';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+const MapContainer = () => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    //develpoment code
+    if(!process.env.REACT_APP_MAPBOX_TOKEN) {
+      return ( <h1 style={{ color: "red"}}>
+        <strong>ERROR!</strong>
+        You havent set the REACT_APP_MAPBOX_TOKEN environmental variable for the Map!
+        Just add "REACT_APP_MAPBOX_TOKEN=*insert token here* to the .env file at the root of the react project!
+        </h1> );
+    }
+  }
+  //production code
+  if(!process.env.REACT_APP_MAPBOX_TOKEN) {
+    console.err("Wow! Looks like we didnt set the mapbox token!");
+    return <div />;
+  }
+  
   return (
-    <section>
-      <img
-        alt=""
-        src="https://www.adventure-journal.com/wp-content/uploads/2016/06/canyonlands-national-park-map-Feat.jpg"
-      />
-    </section>
+    <Map />
+  );
+}
+
+const Map = (props) => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [lng, lat],
+    zoom: zoom
+    });
+  });
+
+  return (
+    <div ref={mapContainer} className="map-container"/>
   );
 };
 
-export default Map;
+export default MapContainer;
