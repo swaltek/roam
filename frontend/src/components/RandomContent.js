42c9ce268a34ee63175ef0894f58ea28e9ccd977
Courtney <courtskit@gmail.com>
2022-05-03 22:38:13 -0700

    switched a tags to Link tags on RandomContent.js

--- frontend/src/components/RandomContent.js
+++ frontend/src/components/RandomContent.js
@@ -1,4 +1,5 @@
 import "../styles/randomContent.css"
+import { Link } from "react-router-dom"
 
 const RandomContent = (props) => {
     return(
@@ -6,17 +7,17 @@ const RandomContent = (props) => {
 
             <section className="container">
 
-                <a className="square1" href="../pages/Blog.js" title="blog"> <h4>Our Blog</h4> </a>
-                
-                <a className="square2" href="../pages/ExpertAdvice.js"> <h4>Expert Advice</h4> </a>
+                <Link className="square1" to="/"> <h4>Our Blog</h4></Link>
 
-                <a className="square3" href="../pages/Listing.js"> <h4>Most Popular Listings</h4> </a>
+                <Link className="square2" to="/"> <h4>Expert Advice</h4></Link>
 
-                <a className="square4" href="../pages/LeaveNoTrace.js"> <h4>Leave No Trace</h4> </a>
+                <Link className="square3" to="/"> <h4>Most Popular Listings</h4></Link>
 
-                <a className="square5" href="../pages/CampingTips.js"> <h4>5 Camping Tips</h4> </a>
+                <Link className="square4" to="/"> <h4>Leave No Trace</h4></Link>
 
-                <a className="square6" href="../pages/Team.js"> <h4>Contact Us</h4> </a>
+                <Link className="square5" to="/"> <h4>5 Camping Tips</h4></Link>
+
+                <Link className="square6" to="/"> <h4>Contact Us</h4></Link>
 
             </section>
 
@@ -25,3 +26,16 @@ const RandomContent = (props) => {
 }
 
 export default RandomContent;
+
+
+{/* <a className="square1" href="../pages/Blog.js" title="blog"> <h4>Our Blog</h4> </a>
+                
+<a className="square2" href="../pages/ExpertAdvice.js"> <h4>Expert Advice</h4> </a>
+
+<a className="square3" href="../pages/Listing.js"> <h4>Most Popular Listings</h4> </a>
+
+<a className="square4" href="../pages/LeaveNoTrace.js"> <h4>Leave No Trace</h4> </a>
+
+<a className="square5" href="../pages/CampingTips.js"> <h4>5 Camping Tips</h4> </a>
+
+<a className="square6" href="../pages/Team.js"> <h4>Contact Us</h4> </a> */}
--- frontend/src/pages/LeaveNoTrace.js
+++ frontend/src/pages/LeaveNoTrace.js
@@ -1,3 +1,5 @@
+import "../styles/leaveNoTrace.css"
+
 export const LeaveNoTrace = () => {
   return (
     <div>
