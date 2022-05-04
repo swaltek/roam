79aa94f25aab0516c94ae39b7b816fad75e0f722
Courtney Smith <62474738+Courtskit@users.noreply.github.com>
2022-05-04 03:21:34 -0700

    featuredParks and randomContent Styling (#10)
    * style randomContent
    
    * style randomContent
    
    * add featured parks functionality
    
    * minor styling changes on featuredParks and randomContent
    
    * switched a tags to Link tags on RandomContent.js
    
    * switched a tags to Link tags on FeaturedParks.js
    
    * minor changes
    
    Co-authored-by: Marcin Swaltek <swaltek@tutanota.com>

--- frontend/src/components/FeaturedParks.js
+++ frontend/src/components/FeaturedParks.js
@@ -1,9 +1,67 @@
+import "../styles/featuredParks.css";
+import { Link } from "react-router-dom";
+
+
 export const FeaturedParks = (props) => {
   return (
     <div>
-      <h1>Featured Parks !</h1>
+      <h2 className="pageHeader centerContent">Featured Parks</h2>
+      <div className="imageGrid">
+        <ul className="centerContent">
+
+          <li className="imageContainer yosemite">
+            <Link to="/"><h4 className="parkNameheader">Yosemite</h4></Link>
+          </li>
+
+          <li className="imageContainer zion">
+            <Link to="/"><h4 className="parkNameheader">Zion</h4></Link>
+          </li>
+
+          <li className="imageContainer grandcanyon">
+            <Link to="/"><h4 className="parkNameheader">Grand Canyon</h4></Link>
+          </li>
+
+          <li className="imageContainer greatsmoky">
+            <Link to="/"><h4 className="parkNameheader">Great Smoky Mountains</h4></Link>
+          </li>
+
+          <li className="imageContainer northcascades">
+            <Link to="/"><h4 className="parkNameheader">North Cascades</h4></Link>
+          </li>
+
+          <li className="imageContainer rockymountains">
+            <Link to="/"><h4 className="parkNameheader">Rocky Mountains</h4></Link>
+          </li>
+
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default FeaturedParks;
+
+// eslint-disable-next-line
+{/* <li className="imageContainer yosemite">
+  <a href="../pages/SearchResults.js" title="Yosemite"><h4>Yosemite</h4></a>
+</li>
+
+<li className="imageContainer zion">
+  <a href="../pages/SearchResults.js" title="Zion"><h4>Zion</h4></a>
+</li>
+
+<li className="imageContainer grandcanyon">
+  <a href="../pages/SearchResults.js" title="Grand Canyon"><h4>Grand Canyon</h4></a>
+</li>
+
+<li className="imageContainer greatsmoky">
+  <a href="../pages/SearchResults.js" title="Great Smoky Mountains"><h4>Great Smoky Mountains</h4></a>
+</li>
+
+<li className="imageContainer northcascades">
+  <a href="../pages/SearchResults.js" title="North Cascades"><h4>North Cascades</h4></a>
+</li>
+
+<li className="imageContainer rockymountains">
+  <a href="../pages/SearchResults.js" title="Rocky Mountains"><h4>Rocky Mountains</h4></a>
+</li> */}
--- frontend/src/components/RandomContent.js
+++ frontend/src/components/RandomContent.js
@@ -1,16 +1,41 @@
+import "../styles/randomContent.css";
+import { Link } from "react-router-dom";
+
 const RandomContent = (props) => {
     return(
-        <div>
-            <section>
-                <a className="photo1" href="../pages/Blog.js" title="blog"><h4>Blog</h4></a>
-                <a className="photo2" href="../pages/Events.js" title="events"><h4>Events</h4></a>
-                <a className="photo3" href="../pages/Listing.js" title="events"><h4>Most Popular Listings</h4></a>
-                <a className="photo4"><h4>Leave No Trace</h4></a>
-                <a className="photo5" href="../pages/CampingTips.js" title="events"><h4>5 Camping Tips</h4></a>
-                <a className="photo6" href="../pages/Charities.js" title="events"><h4>Charities</h4></a>
+        <div className="mainDiv">
+
+            <section className="container">
+
+                <Link className="square1" to="/"> <h4>Our Blog</h4></Link>
+
+                <Link className="square2" to="/"> <h4>Expert Advice</h4></Link>
+
+                <Link className="square3" to="/"> <h4>Most Popular Listings</h4></Link>
+
+                <Link className="square4" to="/"> <h4>Leave No Trace</h4></Link>
+
+                <Link className="square5" to="/"> <h4>5 Camping Tips</h4></Link>
+
+                <Link className="square6" to="/"> <h4>Contact Us</h4></Link>
+
             </section>
+
         </div>
     );
 }
 
 export default RandomContent;
+
+// eslint-disable-next-line
+{/* <a className="square1" href="../pages/Blog.js" title="blog"> <h4>Our Blog</h4> </a>
+                
+<a className="square2" href="../pages/ExpertAdvice.js"> <h4>Expert Advice</h4> </a>
+
+<a className="square3" href="../pages/Listing.js"> <h4>Most Popular Listings</h4> </a>
+
+<a className="square4" href="../pages/LeaveNoTrace.js"> <h4>Leave No Trace</h4> </a>
+
+<a className="square5" href="../pages/CampingTips.js"> <h4>5 Camping Tips</h4> </a>
+
+<a className="square6" href="../pages/Team.js"> <h4>Contact Us</h4> </a> */}
