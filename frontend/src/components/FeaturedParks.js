5e9e8ab7e605ca2f361d1e9e5ced77eb57d753db
Courtney <courtskit@gmail.com>
2022-05-03 21:18:15 -0700

    add featured parks functionality

--- frontend/src/components/FeaturedParks.js
+++ frontend/src/components/FeaturedParks.js
@@ -1,7 +1,40 @@
+import "../styles/featuredParks.css"
+
 export const FeaturedParks = (props) => {
   return (
     <div>
-      <h1>Featured Parks !</h1>
+      
+      <h2 className="pageHeader centerContent">Featured Parks</h2>
+
+      <div className="imageGrid">
+        <ul className="centerContent">
+
+          <li className="imageContainer yosemite">
+            <a href="../pages/SearchResults.js" title="Yosemite"><h4>Yosemite</h4></a>
+          </li>
+
+          <li className="imageContainer zion">
+            <a href="../pages/SearchResults.js" title="Zion"><h4>Zion</h4></a>
+          </li>
+
+          <li className="imageContainer grandcanyon">
+            <a href="../pages/SearchResults.js" title="Grand Canyon"><h4>Grand Canyon</h4></a>
+          </li>
+
+          <li className="imageContainer greatsmoky">
+            <a href="../pages/SearchResults.js" title="Great Smoky Mountains"><h4>Great Smoky Mountains</h4></a>
+          </li>
+
+          <li className="imageContainer northcascades">
+            <a href="../pages/SearchResults.js" title="North Cascades"><h4>North Cascades</h4></a>
+          </li>
+
+          <li className="imageContainer rockymountains">
+            <a href="../pages/SearchResults.js" title="Rocky Mountains"><h4>Rocky Mountains</h4></a>
+          </li>
+        </ul>
+      </div>
+      
     </div>
   );
 };
