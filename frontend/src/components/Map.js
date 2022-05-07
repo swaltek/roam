2f063efe54387946e961a52e3332a1075b542770
Marcin Swaltek <swaltek@tutanota.com>
2022-05-07 07:39:53 -0400

    added visual clusters on map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -47,30 +47,79 @@ const Map = (props) => {
   const setupLoadListings = () => {
     map.current.on('load', () => {
       map.current.addSource('listings', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [],
+        type: 'geojson',
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 50,
+        data: {
+          type: 'FeatureCollection',
+          features: [],
         }
       });
+
       map.current.addLayer({
-          'id': 'listingsLayer',
-          'type': 'symbol',
-          'source': 'listings',
-          'layout': {
+        id: 'clusters',
+        type: 'circle',
+        source: 'listings',
+        filter: ['has', 'point_count'],
+        paint: {
+          // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+          // with three steps to implement three types of circles:
+          //   * Blue, 20px circles when point count is less than 100
+          //   * Yellow, 30px circles when point count is between 100 and 750
+          //   * Pink, 40px circles when point count is greater than or equal to 750
+          'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#51bbd6',
+              100,
+              '#f1f075',
+              750,
+              '#f28cb1'
+            ],
+          'circle-radius': [
+            'step',
+              ['get', 'point_count'],
+              20,
+              100,
+              30,
+              750,
+              40
+          ]
+        }
+      });
+
+      map.current.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'listings',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
+      });
+
+      map.current.addLayer({
+          id: 'unclustered-listing',
+          type: 'symbol',
+          source: 'listings',
+        filter: ['!', ['has', 'point_count']],
+          layout: {
             'icon-image': 'listing-marker',
-            // get the title name from the source's "title" property
+            // get the title name from the sources "title" property
             'text-offset': [0, 1.25],
             'icon-allow-overlap': true,
             'icon-size': 1,
           }
       });
 
-      map.current.on('click', 'listingsLayer', (e) => {
+      map.current.on('click', 'unclustered-listing', (e) => {
         navigate(`/listing/${e.features[0].properties.id}`);
       });
 
-      map.current.on('mouseenter', 'listingsLayer', (e) => {
+      map.current.on('mouseenter', 'unclustered-listing', (e) => {
         map.current.getCanvas().style.cursor = 'pointer';
 
         // Copy coordinates array.
@@ -82,7 +131,7 @@ const Map = (props) => {
           .addTo(map.current);
         });
 
-      map.current.on('mouseleave', 'listingsLayer', () => {
+      map.current.on('mouseleave', 'unclustered-listing', () => {
         map.current.getCanvas().style.cursor = '';
         mapPopup.current.remove();
       });
