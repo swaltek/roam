a6a64f8ebb280a3ad28042d8e1880fb558e83500
Courtney <courtskit@gmail.com>
2022-05-09 12:37:00 -0700

    home page nav styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,13 +8,14 @@ import MostPopularSites from "../components/MostPopularSites";
 import FeaturedParks from "../components/FeaturedParks";
 import RandomContent from "../components/RandomContent";
 import HeaderImage from "../components/HeaderImage";
+import background from "../static/camper.webp"
 
 export const HomePage = (props) => {
   const navigate = useNavigate();
 
   return (
     <VStack>
-      <HeaderImage imageUrl="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg" />
+      <HeaderImage imageUrl={ background } />
       <section className="home-searchbar-section">
         <h1 className="home-searchbar-title">Your next adventure awaits...</h1>
         <form className="home-searchbar-form">
@@ -27,11 +28,11 @@ export const HomePage = (props) => {
           }}/>
         </form>
       </section>
-      <section className="map-section">
+      {/* <section className="map-section">
         <div className="map-content-container">
           <Map loadListings/>
         </div>
-      </section>
+      </section> */}
       <MostPopularSites user={props.user} setUser={props.setUser}/>
 
       {/* <BoondockSites /> */}
