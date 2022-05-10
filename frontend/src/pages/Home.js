11596f17ca39750cf497b12bfd2f528731cc8eef
leperrine <leperrine@gmail.com>
2022-05-10 10:36:10 -0500

    updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { VStack } from "@chakra-ui/react";
-import { useNavigate } from 'react-router-dom';
-import "../styles/Home.css"
+import { useNavigate } from "react-router-dom";
+import "../styles/Home.css";
 import Map from "../components/Map";
 import Search from "../components/GeocodingSearch";
 import MostPopularSites from "../components/MostPopularSites";
@@ -8,31 +8,27 @@ import MostPopularSites from "../components/MostPopularSites";
 import FeaturedParks from "../components/FeaturedParks";
 import RandomContent from "../components/RandomContent";
 import HeaderImage from "../components/HeaderImage";
-import background from "../static/camper.webp"
+import background from "../static/camper.webp";
 
 export const HomePage = (props) => {
   const navigate = useNavigate();
 
   return (
     <VStack>
-      <HeaderImage imageUrl={ background } />
+      <HeaderImage imageUrl={background} />
       <section className="home-searchbar-section">
         <h1 className="home-searchbar-title">Your next adventure awaits...</h1>
         <form className="home-searchbar-form">
-          <Search onSearch={(e, searchData) => {
-            navigate(`/listing/search`,
-              {
+          <Search
+            onSearch={(e, searchData) => {
+              navigate(`/listing/search`, {
                 state: searchData,
               });
-          }}/>
+            }}
+          />
         </form>
       </section>
-      {/* <section className="map-section">
-        <div className="map-content-container">
-          <Map loadListings/>
-        </div>
-      </section> */}
-      <MostPopularSites user={props.user} setUser={props.setUser}/>
+      <MostPopularSites user={props.user} setUser={props.setUser} />
 
       {/* <BoondockSites /> */}
 
