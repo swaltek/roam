c0b9dc27713125c59f732199c1429f5fe0063cae
leperrine <leperrine@gmail.com>
2022-05-10 09:38:26 -0500

    updates

--- frontend/src/pages/AccountPage/ReservationDetails.js
+++ frontend/src/pages/AccountPage/ReservationDetails.js
@@ -35,7 +35,6 @@ function ReservationDetails() {
       };
 
       return (
-<<<<<<< HEAD
         <GridItem>
           <ReservationCard
             key={reservation.id}
@@ -50,24 +49,8 @@ function ReservationDetails() {
       );
     });
   };
-=======
-        <div key={`res_${index}`} className="box neutral2 padding text-center ">
-            <h4 className="heavyText genericSecondaryHeader">{reservation.listing_name}</h4>
-            <h4>{reservation.date_start}</h4>
-            <h4>{reservation.date_end}</h4>
-            <h4>${reservation.total}</h4>
-            <button className="button-4" onClick={ handleDeleteReservation }>Cancel</button>
-        </div>
-      )
-    })
-  }
->>>>>>> a636b7d3d4b4cd7e2922ae9a3a5327ac8a95ef1c
 
-  return (
-    <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-      {renderReservations()}
-    </Grid>
-  );
+  return <Grid autoRows={"auto"}>{renderReservations()}</Grid>;
 }
 
 export default ReservationDetails;
