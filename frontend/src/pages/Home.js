97df54b098a184c8b540bf48d5012b238ee0551e
Marcin Swaltek <swaltek@tutanota.com>
2022-05-11 08:15:56 -0400

    actually fixed it this time

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,4 @@
 import { VStack } from "@chakra-ui/react";
-import { useNavigate } from 'react-router-dom';
-import "../styles/Home.css"
 import { useNavigate } from "react-router-dom";
 import "../styles/Home.css";
 import Search from "../components/GeocodingSearch";
