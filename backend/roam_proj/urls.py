ad1ed873fa0c65969d0548f9f58a887aec0d85f9
Marcin Swaltek <swaltek@tutanota.com>
2022-05-21 09:00:57 -0400

    fixed urls for react_router

--- backend/roam_proj/urls.py
+++ backend/roam_proj/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .view import send_the_homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', send_the_homepage),
-    path('api/', include('roam_app.urls'))
+    path('api/', include('roam_app.urls')),
+    re_path(r'.*', send_the_homepage), #re path so react_router can do its thing
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
